cb6e7d6224b0d8c44cb905fd462d77fa37b74f47
Curious-Eye <andrey.lupinos@gmail.com>
2023-09-29 10:12:49 +0300

    Fix logout not deleting cookies

--- frontend/pages/logout.vue
+++ frontend/pages/logout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 function eraseCookie(name: string) {
-  document.cookie = name+'=; Max-Age=-99999999;';
+  document.cookie = name+'=; Max-Age=0';
 }
 
 if (process.client) {
